84ce0f88442032035fcd84011c39a18bc048cd85
Simone Cesano <simonecesano@libero.it>
2023-04-06 19:34:56 +0200

    Added docs

--- lib/Role/Tiny/MonkeyPatch.pm
+++ lib/Role/Tiny/MonkeyPatch.pm
@@ -71,3 +71,84 @@ sub import {
 
 1;
 
+=pod
+
+=head1 Role::Tiny::MonkeyPatch
+
+=head2 NAME
+
+Role::Tiny::MonkeyPatch - monkeypatch roles into existing classes
+
+=head2 SYNOPSIS
+
+    use Role::Tiny::MonkeyPatch qw/Some::Existing::Class/;
+
+    my $something = Some::Existing::Class->new->withroles('+Some::Role');
+
+    $something->some_method_provided_by_role
+
+or
+
+    use Role::Tiny::MonkeyPatch;
+
+    my $something = Some::Existing::Class->new;
+
+    monkey_patch_roles($something);
+
+    $something->some_method_provided_by_role
+
+or
+
+    use Role::Tiny::MonkeyPatch;
+
+    monkey_patch_roles(Some::Existing::Class);
+
+    my $something = Some::Existing::Class->new;
+
+    $something->some_method_provided_by_role
+
+
+=head2 DESCRIPTION
+
+Role::Tiny::MonkeyPatch monkey patches Role::Tiny-based roles onto existing modules
+
+=head2 FUNCTIONS
+
+Role::Tiny::MonkeyPatch exports only one function
+
+=head3 monkey_patch_roles
+
+    monkey_patch_roles(Some::Class);
+
+or 
+
+    my $obj = Some::Class->new;
+    monkey_patch_roles($obj);
+
+monkey patch roles onto an existing class or object
+
+
+
+=head2 CAVEATS AND NOTES
+
+Be aware that Mojo::Base roles only work on hash-based classes
+
+=head2 SEE ALSO
+
+=over
+
+=item Role::Tiny
+
+=item Mojo::Util
+
+=back
+
+=head2 AUTHORS
+
+Simone Cesano
+
+=head2 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2023 by Simone Cesano.
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
